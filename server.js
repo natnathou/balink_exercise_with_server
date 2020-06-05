86b9f44a1c27a41d98634d8550b9e10bf7c27de5
nat z <ne.zaffran@gmail.com>
2020-06-05 08:56:49 +0300

    remove body-parser

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/user/new', (req, res) => {
     res.status(200).end();
@@ -15,11 +12,5 @@ app.get('/country', (req, res) => {
 });
 
 
-app.post('/api/world', (req, res) => {
-    console.log(req.body);
-    res.send(
-        `I received your POST request. This is what you sent me: ${req.body.post}`,
-    );
-});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
